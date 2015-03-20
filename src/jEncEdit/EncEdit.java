8febef9b8fb13c70cbc498e03928274d91d65438
Vladimir Valeyev <ValV@users.noreply.github.com>
2015-03-20 08:49:37 +0600

    Add Eclipse Project

---
+++ src/jEncEdit/EncEdit.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package jEncEdit;
+
+/**
+ * @author umbrafer
+ *
+ */
+public class EncEdit {
+
+	/**
+	 * 
+	 */
+	public EncEdit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
